cc57fc4e3903ef19556d6273969973ca036f5337
gustavo-tomas <guga.tomas10@gmail.com>
2023-01-15 10:15:48 -0300

    Completa a gramática e adiciona testes de unidade

--- src/main.c
+++ src/main.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "test.h"
 
 void parse_file(char file[]);
 
 int main(int argc, char **argv) {
 
-    if (argc < 2) {
-        printf("Nenhum arquivo fornecido\n");
-        exit(1);
-    }
-    
-    parse_file(argv[1]);
+    if (argc >= 2)
+        parse_file(argv[1]);
+
+    else
+        run_all_tests();
 
     return 0;
 }
