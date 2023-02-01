0dfff01ea44f8a7a8346656d97032000d0439f85
gustavo-tomas <guga.tomas10@gmail.com>
2023-02-01 11:05:48 -0300

    Adiciona suporte para escrita/leitura de float

--- src/sintatico/sintatico.y
+++ src/sintatico/sintatico.y
@@ -13,9 +13,10 @@ void yyerror(char*);
 int yylex(void);
 %}
 
-%union { // SEMANTIC RECORD
-    int intval; // Valor inteiro
-    char* id; // Identificadores
+%union {            // Acrescentar tipos aqui
+    int intval;     // Inteiro
+    float floatval; // Float 
+    char* id;       // Identificadores
     struct lbs* lbls;
 } 
 
@@ -30,11 +31,14 @@ int yylex(void);
 %token IN 
 %token INTEGER 
 %token LET 
-%token <intval> NUMBER
+%token <intval> NUMBER_INTEGER
+%token <floatval> NUMBER_FLOAT
 %token READ 
+%token READ_FT
 %token SKIP
 %token THEN
 %token WRITE
+%token WRITE_FT
 
 %left '-' '+'
 %left '*' '/'
@@ -44,9 +48,9 @@ int yylex(void);
 
 program:        LET 
                     declarations
-                IN                                              {gen_code(DATA, sym_table->offset);}
+                IN                                              {gen_code(DATA, (union stack_t) {.intval = sym_table->offset});}
                     commands
-                END                                             {gen_code(HALT, 0); 
+                END                                             {gen_code(HALT, (union stack_t) {.intval =  0}); 
                                                                  fetch_execute_cycle();
                                                                  clear_table(sym_table);
                                                                  clear_label_list(lbs_list);
@@ -55,13 +59,13 @@ program:        LET
                                                                  YYACCEPT;}
                 ;
 
-id_seq:         /* empty */
-                | id_seq IDENTIFIER ','                         {install($2);}
-                ;
-
 declarations:   /* empty */                                     {install("0");}
                 | declarations INTEGER id_seq IDENTIFIER ';'    {install($4);}
                 | declarations FLOAT id_seq IDENTIFIER ';'      {install($4);}
+                ;
+
+id_seq:         /* empty */
+                | id_seq IDENTIFIER ','                         {install($2);}
                 ;
 
 commands:       /* empty */
@@ -70,31 +74,35 @@ commands:       /* empty */
 
 command:        SKIP
                 | READ IDENTIFIER                               {context_check(READ_INT, $2);}
-                | WRITE exp                                     {gen_code(WRITE_INT, 0);}
+                | READ_FT IDENTIFIER                            {context_check(READ_FLOAT, $2);}
+                | WRITE exp                                     {gen_code(WRITE_INT, (union stack_t) {.intval = 0});}
+                | WRITE_FT exp                                  {gen_code(WRITE_FLOAT, (union stack_t) {.floatval = 0});}
                 | IDENTIFIER ASSGNOP exp                        {context_check(STORE, $1);}
                 | IF exp                                        {$1 = (struct lbs *) create_label(); $1->for_jmp_false = reserve_loc();}
                     THEN commands                               {$1->for_goto = reserve_loc();}
-                    ELSE                                        {back_patch($1->for_jmp_false, JMP_FALSE, gen_label());}
+                  ELSE                                          {back_patch($1->for_jmp_false, JMP_FALSE, (union stack_t) {.intval = gen_label()});}
                         commands
-                  END                                           {back_patch($1->for_goto, GOTO, gen_label());}
+                  END                                           {back_patch($1->for_goto, GOTO, (union stack_t) {.intval = gen_label()});}
                 /* | IF exp THEN commands END                      {;} /* @TODO: Ver como esse aqui funciona */
                 | WHILE                                         {$1 = (struct lbs *) create_label(); $1->for_goto = gen_label();}
                     exp                                         {$1->for_jmp_false = reserve_loc();}
                   DO
                     commands
-                  END                                           {gen_code(GOTO, $1->for_goto); back_patch($1->for_jmp_false, JMP_FALSE, gen_label());}
+                  END                                           {gen_code(GOTO, (union stack_t) {.intval = $1->for_goto}); 
+                                                                 back_patch($1->for_jmp_false, JMP_FALSE, (union stack_t) {.intval = gen_label()});}
                 ;
 
-exp:            NUMBER                                          {gen_code(LD_INT, $1);}
+exp:            NUMBER_INTEGER                                  {gen_code(LD_INT, (union stack_t) {.intval = $1});}
+                | NUMBER_FLOAT                                  {gen_code(LD_FLOAT, (union stack_t) {.floatval = $1});}
                 | IDENTIFIER                                    {context_check(LD_VAR, $1);}
-                | exp exp '<'                                   {gen_code(LT, 0);}
-                | exp exp '='                                   {gen_code(EQ, 0);}
-                | exp exp '>'                                   {gen_code(GT, 0);}
-                | exp exp '+'                                   {gen_code(ADD, 0);}
-                | exp exp '-'                                   {gen_code(SUB, 0);}
-                | exp exp '*'                                   {gen_code(MULT, 0);}
-                | exp exp '/'                                   {gen_code(DIV, 0);}
-                | exp exp '^'                                   {gen_code(PWR, 0);}
+                | exp exp '<'                                   {gen_code(LT, (union stack_t) {.intval = 0});}
+                | exp exp '='                                   {gen_code(EQ, (union stack_t) {.intval = 0});}
+                | exp exp '>'                                   {gen_code(GT, (union stack_t) {.intval = 0});}
+                | exp exp '+'                                   {gen_code(ADD, (union stack_t) {.intval = 0});}
+                | exp exp '-'                                   {gen_code(SUB, (union stack_t) {.intval = 0});}
+                | exp exp '*'                                   {gen_code(MULT, (union stack_t) {.intval = 0});}
+                | exp exp '/'                                   {gen_code(DIV, (union stack_t) {.intval = 0});}
+                | exp exp '^'                                   {gen_code(PWR, (union stack_t) {.intval = 0});}
                 | '(' exp ')'
                 ;
 
--- src/stack_machine.h
+++ src/stack_machine.h
@@ -6,23 +6,29 @@
 
 // Representacao interna das operacoes
 enum code_ops { HALT, STORE, JMP_FALSE, GOTO,
-                DATA, LD_INT, LD_VAR,
-                READ_INT, WRITE_INT,
+                DATA, LD_INT, LD_FLOAT, LD_VAR,
+                READ_INT, READ_FLOAT, WRITE_INT, WRITE_FLOAT,
                 LT, EQ, GT, ADD, SUB, MULT, DIV, PWR };
 
 // Representacao externa das operacoes
 static char* op_name[] = { "halt", "store", "jmp_false", "goto",
-                           "data", "ld_int", "ld_var",
-                           "in_int", "out_int",
+                           "data", "ld_int", "ld_float", "ld_var",
+                           "in_int", "in_float", "out_int", "out_float",
                            "lt", "eq", "gt", "add", "sub", "mult", "div", "pwr" };
+
+// Tipos de dado da pilha
+static union stack_t {
+    int intval;
+    float floatval;
+} stack_t;
 
 struct instruction {
     enum code_ops op;
-    int arg;
+    union stack_t arg;
 };
 
 static struct instruction code[999]; // Armazena programa (read only)
-static int stack[999];               // Armazena dados
+static union stack_t stack[999];     // Armazena dados
 
 static struct instruction ir;        // Registrador de instrucao (Instruction Register)
 static int pc = 0;                   // Registrador de endereço de programa (Program Counter)
@@ -36,80 +42,90 @@ static inline void fetch_execute_cycle() {
     do {
         // Carrega
         ir = code[pc++];
-        fprintf(object_code, "%2d: %-12s%d\n", pc - 1, op_name[ir.op], ir.arg);
+        fprintf(object_code, "%2d: %-12s%d\n", pc - 1, op_name[ir.op], ir.arg.intval);
 
         // Executa
         switch (ir.op) {
-            case HALT:      printf("Halt\n"); 
-                            break;
+            case HALT:          printf("Halt\n"); 
+                                break;
 
-            case READ_INT:  printf("Input: ");
-                            scanf("%d", &stack[ar+ir.arg]); 
-                            break;
+            case READ_INT:      printf("Input: ");
+                                scanf("%d", &stack[ar+ir.arg.intval].intval);
+                                break;
 
-            case WRITE_INT: printf("Output: %d\n", stack[top--]); 
-                            break;
+            case READ_FLOAT:    printf("Finput: ");
+                                scanf("%f", &stack[ar+ir.arg.intval].floatval); 
+                                break;
 
-            case STORE:     stack[ir.arg] = stack[top--]; 
-                            break;
+            case WRITE_INT:     printf("Output: %d\n", stack[top--].intval);
+                                break;
 
-            case JMP_FALSE: if (stack[top--] == 0)
-                                pc = ir.arg;
-                            break;
+            case WRITE_FLOAT:   printf("Floatput: %f\n", stack[top--].floatval);
+                                break;
 
-            case GOTO:      pc = ir.arg; 
-                            break;
+            case STORE:         stack[ir.arg.intval] = stack[top--]; 
+                                break;
 
-            case DATA:      top = top + ir.arg;
-                            break;
+            case JMP_FALSE:     if (stack[top--].intval == 0)
+                                    pc = ir.arg.intval;
+                                break;
 
-            case LD_INT:    stack[++top] = ir.arg;  
-                            break;
+            case GOTO:          pc = ir.arg.intval;
+                                break;
 
-            case LD_VAR:    stack[++top] = stack[ar+ir.arg];
-                            break;
+            case DATA:          top = top + ir.arg.intval;
+                                break;
 
-            case LT:        if (stack[top-1] < stack[top])
-                                stack[--top] = 1;
-                            else
-                                stack[--top] = 0;
-                            break;
+            case LD_INT:        stack[++top] = ir.arg;
+                                break;
 
-            case EQ:        if (stack[top-1] == stack[top])
-                                stack[--top] = 1;
-                            else
-                                stack[--top] = 0;
-                            break;
+            case LD_FLOAT:      stack[++top] = ir.arg;
+                                break;
 
-            case GT:        if (stack[top-1] > stack[top])
-                                stack[--top] = 1;
-                            else
-                                stack[--top] = 0;
-                            top--;
-                            break;
+            case LD_VAR:        stack[++top] = stack[ar+ir.arg.intval];
+                                break;
 
-            case ADD:       stack[top-1] = stack[top-1] + stack[top];
-                            top--;
-                            break;
+            case LT:            if (stack[top-1].intval < stack[top].intval)
+                                    stack[--top].intval = 1;
+                                else
+                                    stack[--top].intval = 0;
+                                break;
 
-            case SUB:       stack[top-1] = stack[top-1] - stack[top];
-                            top--;
-                            break;
+            case EQ:            if (stack[top-1].intval == stack[top].intval)
+                                    stack[--top].intval = 1;
+                                else
+                                    stack[--top].intval = 0;
+                                break;
 
-            case MULT:      stack[top-1] = stack[top-1] * stack[top];
-                            top--;
-                            break;
+            case GT:            if (stack[top-1].intval > stack[top].intval)
+                                    stack[--top].intval = 1;
+                                else
+                                    stack[--top].intval = 0;
+                                top--;
+                                break;
 
-            case DIV:       stack[top-1] = stack[top-1] / stack[top];
-                            top--;
-                            break;
+            case ADD:           stack[top-1].intval = stack[top-1].intval + stack[top].intval;
+                                top--;
+                                break;
 
-            case PWR:       stack[top-1] = stack[top-1] * stack[top];
-                            top--;
-                            break;
+            case SUB:           stack[top-1].intval = stack[top-1].intval - stack[top].intval;
+                                top--;
+                                break;
 
-            default:        printf("Internal Error: Memory Dump\n");
-                            break;
+            case MULT:          stack[top-1].intval = stack[top-1].intval * stack[top].intval;
+                                top--;
+                                break;
+
+            case DIV:           stack[top-1].intval = stack[top-1].intval / stack[top].intval;
+                                top--;
+                                break;
+
+            case PWR:           stack[top-1].intval = stack[top-1].intval * stack[top].intval;
+                                top--;
+                                break;
+
+            default:            printf("Internal Error: Memory Dump\n");
+                                break;
         }
     } while (ir.op != HALT);
 
